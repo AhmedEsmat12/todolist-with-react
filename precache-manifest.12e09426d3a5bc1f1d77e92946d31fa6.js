4e45e819b15616989a264a009bbe79a3ee6ea86f
شاةثي <@.>
2019-07-27 19:31:09 +0200

    Updates

---
+++ precache-manifest.12e09426d3a5bc1f1d77e92946d31fa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858395dd699e44697fa4bb5c51d81f8f",
+    "url": "/todolist-with-react/index.html"
+  },
+  {
+    "revision": "120046aefacdb813dad9",
+    "url": "/todolist-with-react/static/css/main.49e3b9b3.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/todolist-with-react/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "120046aefacdb813dad9",
+    "url": "/todolist-with-react/static/js/main.6f815936.chunk.js"
+  },
+  {
+    "revision": "6fad26131a7a70574815",
+    "url": "/todolist-with-react/static/js/runtime~main.7d565943.js"
+  }
+]);
